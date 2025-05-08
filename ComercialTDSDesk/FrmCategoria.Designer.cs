44d4eae5287035c5dec875043df1ead114c87928
RAFAEL SANTANA FIGUEIRO <rafael.sfigueiro@senacsp.edu.br>
2025-05-08 11:50:50 -0300

    Adicionar arquivos de projeto.

---
+++ ComercialTDSDesk/FrmCategoria.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace ComercialTDSDesk
+{
+    partial class FrmCategoria
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // FrmCategoria
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Name = "FrmCategoria";
+            Text = "FrmCategoria";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
